57454be1add8e831e446129fcc4d47fe9068b882
Mel-Rocha <rochamel73@gmail.com>
2023-05-20 22:02:43 -0300

    Padrão MVC, estrutura de projeto.

---
+++ SRC/Controller/Controller.py
@@ -0,0 +1,5 @@
+from ..Model.Model import hello_world
+from ..View.View import start_gui
+
+start_gui()
+hello_world()
---
+++ SRC/Model/Model.py
@@ -0,0 +1,4 @@
+"""Coloque as funções e a lógica da aplicação aqui"""
+
+def hello_world():
+    print('Hello World')
